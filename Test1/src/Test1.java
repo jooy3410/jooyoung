16d473ab530eb89f9e0b0f52f62cb620bca98a3e
Once <Once@DESKTOP-SEKFJRU>
2020-05-28 23:51:34 +0900

    .

---
+++ Test1/src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int i, k;
+		for (i=2;i<=9;i++) {
+			for(k=1;k<=9;k++)
+				System.out.printf("%d*%d=%d \n",i,k,(i*k));
+			
+		
+		System.out.printf("\n");
+		}
+	}
+
+}
